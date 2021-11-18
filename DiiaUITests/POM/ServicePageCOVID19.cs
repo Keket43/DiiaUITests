7e6c853d3e1ec9b9881e758b9b27e530fb2b54a4
HOME-PC\Ket <ket.bodnarchuk@gmail.com>
2021-11-18 14:34:16 +0200

    Add SearchSteps

---
+++ DiiaUITests/POM/ServicePageCOVID19.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiiaUITests.POM
+{
+    class ServicePageCOVID19
+    {
+        private readonly IWebDriver _webDriver;
+
+        public  ServicePageCOVID19(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        private readonly By _title = By.CssSelector(".service-type_title");
+
+
+        public string GetTitleText()
+        {
+            return _webDriver.FindElement(_title).Text;
+            
+        }
+    }
+}
