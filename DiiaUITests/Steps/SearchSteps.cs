67f8b4d885394f6f0ce44d93f285c456c80c0735
HOME-PC\Ket <ket.bodnarchuk@gmail.com>
2021-11-18 12:49:37 +0200

    Add SearchSteps

---
+++ DiiaUITests/Steps/SearchSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace DiiaUITests.Steps
+{
+    [Binding]
+    public class SearchSteps
+    {
+        [When(@"I input ""(.*)"" in search field")]
+        public void WhenIInputInSearchField(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
